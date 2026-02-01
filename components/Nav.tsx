0fcba074f838cf9ee2209c3143caec7a1deae12e
Alex Bussey <awbussey83@gmail.com>
2026-02-01 15:34:28 -0500

    Update next/link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,13 +5,17 @@ import { Options } from "./Options";
 export function Nav() {
   return (
     <nav className="flex items-center justify-between h-12">
-      <Link passHref href="/" legacyBehavior>
+      <Link href="/">
+        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+        }
         <div className="text-2xl font-bold text-accent cursor-pointer">
           Alex Bussey
         </div>
       </Link>
       <ul className="flex items-center space-x-8">
-        <Link href="/resume" passHref legacyBehavior>
+        <Link href="/resume">
+          {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+          }
           <span className="text-accent border-accent rounded-md border-2 py-2 px-4 cursor-pointer hover:text-dark hover:bg-accent dark:hover:text-primary">
             Resumé
           </span>
